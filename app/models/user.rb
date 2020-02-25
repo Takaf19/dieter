86f0d4ffcec136384ea93f580a4694a8194f403b
Takaf19 <tk.f19ms@gmail.com>
2020-02-25 21:35:00 +0900

    create models
    models:
    comment
    group
    message
    status
    user

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  
+  belongs_to :status
+  has_many :groups
+  has_many :messages
+  has_many :comments
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+end
