3d5f0c806a95e7184aeb430ea1356413115390e8
Takaf19 <tk.f19ms@gmail.com>
2020-03-03 18:07:32 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {   registrations: 'users/registrations',
+                                    sessions: 'users/sessions' }
+  root "top#index"
+  resources :users, only: :show do
+    resources :statuses, only: [:index]
+  end
+
+  resources :statuses, only: [:new, :create, :edit, :update]
+  resources :groups, only: [:new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
